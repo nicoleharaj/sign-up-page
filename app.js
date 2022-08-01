4091e61d1b6cea0fcb38fe8c991b27b35814247e
Nicole Haraj <nicoleharaj@outlook.com>
2022-07-31 21:31:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ let onChange = () => {
   const password = document.querySelector('input[name=password]')
   const confirm = document.querySelector('input[name=confirm-password]')
 
-  if (confirm.value === password.value) {
-    confirm.setCustomValidity('');
-  } else {
-    confirm.setCustomValidity('Passwords do not match.');
-  }
+  confirm.value === password.value ? confirm.setCustomValidity('') : confirm.setCustomValidity('Passwords do not match.')
 }
-
 
 document.querySelectorAll('.form-input').forEach((e) => {
   e.addEventListener(
@@ -20,8 +15,12 @@ document.querySelectorAll('.form-input').forEach((e) => {
   )
 })
 
-document.querySelector('#submit-button').addEventListener('click', () => {
-  document.querySelectorAll('.form-input').forEach((e) => {
-    e.classList.add('submitted')
-  })
-}, false)
+document.querySelector('#submit-button').addEventListener(
+  'click',
+  () => {
+    document.querySelectorAll('.form-input').forEach((e) => {
+      e.classList.add('submitted')
+    })
+  },
+  false
+)
